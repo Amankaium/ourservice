6dc39b844b7bf6089ff4678091002c1c7ec6f7d6
Amankaium <kaium@list.ru>
2022-05-18 11:58:50 +0600

    add media

--- master/models.py
+++ master/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Master(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
+    price = models.IntegerField(default=0)  
+    photo = models.ImageField(null=True, blank=True)
--- master/serializers.py
+++ master/serializers.py
@@ -4,4 +4,4 @@ from .models import Master
 class MasterSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Master
-        fields = ['id', 'name', 'description']
+        fields = ['id', 'name', 'description', "price", "photo"]
